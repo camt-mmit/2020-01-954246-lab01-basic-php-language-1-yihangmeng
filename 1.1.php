cda4780643ebcf2fced8815547bdca7c1f0f1a0e
yihangmeng <1441270251@qq.com>
2020-10-30 15:36:55 +0000

    finish assignment1

--- 1.1.php
+++
@@ -1,6 +0,0 @@
-<?php
-printf("input your price and discount:");
-fscanf(STDIN, "%d %d", $price,$discount);
-$decrease=$price*$discount/100;
-$net=$price-$decrease;
-printf("price: %.2f, discount: %.2f, net price: %.2f",$price,$discount,$net);
